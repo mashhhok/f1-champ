e72024482c3294665c43810e1c63f298a6f630c8
Mariia <71460862+mashhhok@users.noreply.github.com>
2025-05-19 12:59:54 +0200

    Update getDrivers.ts

--- controllers/getDrivers.ts
+++ controllers/getDrivers.ts
@@ -3,36 +3,49 @@ import Driver from "../models/Drivers";
 
 export const getDrivers = async (req: any, res: any) => {
     const { seasons } = req.params;
-
+  
     const urls = [
-        "https://api.jolpi.ca/ergast/f1/2025/results/",
-        "https://api.jolpi.ca/ergast/f1/2025/results/?offset=30",
-        "https://api.jolpi.ca/ergast/f1/2025/results/?offset=60",
-        "https://api.jolpi.ca/ergast/f1/2025/results/?offset=90",
-        "https://api.jolpi.ca/ergast/f1/2025/results/?offset=120"
-      ];
-    
-    const responses = await Promise.all(urls.map(url => axios.get(url)));
+      "https://api.jolpi.ca/ergast/f1/2025/results/",
+      "https://api.jolpi.ca/ergast/f1/2025/results/?offset=30",
+      "https://api.jolpi.ca/ergast/f1/2025/results/?offset=60",
+      "https://api.jolpi.ca/ergast/f1/2025/results/?offset=90",
+      "https://api.jolpi.ca/ergast/f1/2025/results/?offset=120"
+    ];
+  
+  
+      const responses = await Promise.all(urls.map((url) => axios.get(url)));
+      const responseData = responses.map((response) => response.data);
+  
+      const driverArray: any[] = [];
+  
+      responseData.forEach((raceData: any) => {
+        const races = raceData.MRData.RaceTable.Races;
 
-    const responseData = responses.map((response) => response.data);
-
-    const driverArray = responseData.map((race: any) => {
-        console.log(race.MRData.RaceTable.Races);
-        return {
-            raceName: [race.MRData.RaceTable.Races[0].raceName],
-            season: race.MRData.RaceTable.Races[0].season,
-            givenName: race.MRData.RaceTable.Races[0].Results[0].Driver.givenName,
-            familyName: race.MRData.RaceTable.Races[0].Results[0].Driver.familyName,
-            dateOfBirth: race.MRData.RaceTable.Races[0].Results[0].Driver.dateOfBirth,
-            nationality: race.MRData.RaceTable.Races[0].Results[0].Driver.nationality
-        }
-    })
-
-    await Driver.insertMany(driverArray);
-
-
-    const drivers = await Driver.find({ season: seasons });
-    res.json(drivers);
-}
-
-
+        const winners = races.results.map((result: any) => result.position == 1);
+  
+        races.forEach((race: any) => {
+        
+          const raceName = race.raceName;
+          const season = race.season;
+  
+          const winnerResult = race.Results[0];
+          const driver = winnerResult.Driver;
+  
+          driverArray.push({
+            raceName,
+            season,
+            givenName: driver.givenName,
+            familyName: driver.familyName,
+            dateOfBirth: driver.dateOfBirth,
+            nationality: driver.nationality,
+          });
+        });
+      });
+  
+      await Driver.insertMany(driverArray);
+  
+  
+      const drivers = await Driver.find({ season: seasons });
+      res.json(drivers);
+  
+  };
