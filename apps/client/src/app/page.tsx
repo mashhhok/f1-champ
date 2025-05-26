6dccacd15a74d677848cdb3cd0fcdc9c313ec21a
Mariia Riabikova <ryabikovamari003@gmail.com>
2025-05-26 21:36:17 +0200

    Update main page wording

--- apps/client/src/app/page.tsx
+++ apps/client/src/app/page.tsx
@@ -61,7 +61,7 @@ export default function Index() {
                   textAlign: 'center'
                 }}
               >
-                World Champions by Season
+                World Champions by Season!
               </Typography>
               <SeasonsContainer />
             </Box>
